d0902e027e2efde0bb30793326e058189bd117c0
tusharshaha <tusharshaha128@gmail.com>
2022-03-31 22:14:18 +0600

    created short details on home page

---
+++ src/Components/Home/ShortDetails/ShortDetails.tsx
@@ -0,0 +1,42 @@
+import React, { ReactElement } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { AiOutlineRocket, AiOutlineGift } from 'react-icons/ai';
+import { BsHeadset, BsPiggyBank } from 'react-icons/bs';
+import './ShortDetails.css';
+
+interface details {
+    icon: ReactElement,
+    title: string,
+    subTitle: string
+}
+const ShortDetails: React.FC = () => {
+    const stDetails: details[] = [
+        { icon: <AiOutlineRocket />, title: 'FREE DELIVERY', subTitle: 'Free shipping on all order' },
+        { icon: <BsHeadset />, title: 'ONLINE SUPPORT 24/7', subTitle: 'Support online 24 hours a day' },
+        { icon: <BsPiggyBank />, title: 'MONEY RETURN', subTitle: 'Back guarantee under 7 days' },
+        { icon: <AiOutlineGift />, title: 'MEMBER DISCOUNT', subTitle: 'Onevery order over $120.00' }
+    ]
+    return (
+        <div className='my-5 border '>
+            <div className='py-5 px-4'>
+                <Row xs={1} md={2} lg={4} className="g-4 mx-auto">
+                    {
+                        stDetails.map((dt, i) => <Col key={i}>
+                            <div className='d-flex'>
+                                <div className="details_icon">
+                                    {dt.icon}
+                                </div>
+                                <div>
+                                    <p className='fw-bold mb-1'>{dt.title}</p>
+                                    <p className='mb-0'>{dt.subTitle}</p>
+                                </div>
+                            </div>
+                        </Col>)
+                    }
+                </Row>
+            </div>
+        </div>
+    );
+};
+
+export default ShortDetails;
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../../Components/Home/Header/Header';
 import NavBar from '../../Components/Home/NavBar/NavBar';
 import ShortCategory from '../../Components/Home/ShortCategory/ShortCategory';
+import ShortDetails from '../../Components/Home/ShortDetails/ShortDetails';
 
 const Home : React.FC = () => {
     return (
@@ -9,6 +10,7 @@ const Home : React.FC = () => {
             <NavBar></NavBar>
             <Header></Header>
             <ShortCategory></ShortCategory>
+            <ShortDetails></ShortDetails>
         </>
     );
 };
